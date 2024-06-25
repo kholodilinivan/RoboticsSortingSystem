585db068907f66849e42a4484856b2bf6da6dfa6
kholodilinivan <79514433311@yandex.ru>
2024-06-25 12:37:14 +0500

    updates

--- MatlabProject/4_Tutorial_Advance_Chess/cube_dist.m
+++ MatlabProject/4_Tutorial_Advance_Chess/cube_dist.m
@@ -11,13 +11,13 @@ for i=i_(1):i_(2)     % working image region
     for j=j_(1):j_(2)
         if I(j,i)>0   
             m=[j;i];       % image pixels
-            M = cam2world(m,ocam); % transform from image plane to the camera plane          
-            a1 = M(1)*r(2,1)-M(2)*r(1,1);
-            b1 = M(1)*r(2,2)-M(2)*r(1,2);
-            c1 = M(1)*r(2,3)-M(2)*r(1,3);
-            a2 = M(3)*r(1,1)-M(1)*r(3,1);
-            b2 = M(3)*r(1,2)-M(1)*r(3,2);
-            c2 = M(3)*r(1,3)-M(1)*r(3,3);
+            M = cam2world(m,ocam); % transform from image plane to the camera plane   
+            a1 = M(3)*r(1,1)-M(1)*r(3,1);
+            b1 = M(3)*r(1,2)-M(1)*r(3,2);
+            c1 = M(3)*r(1,3)-M(1)*r(3,3);       
+            a2 = M(1)*r(2,1)-M(2)*r(1,1);
+            b2 = M(1)*r(2,2)-M(2)*r(1,2);
+            c2 = M(1)*r(2,3)-M(2)*r(1,3);
  
             Y = (a2*c1-a1*c2)/(a1*b2-a2*b1);
             X = (-c1-b1*Y)/a1;
--- MatlabProject/4_Tutorial_Advance_Chess/mapping.m
+++ MatlabProject/4_Tutorial_Advance_Chess/mapping.m
@@ -13,13 +13,13 @@ for i=1:height      % working image region
     for j=1:width
         if image(i,j)>0
             m=[i;j];       % image pixels
-            M = cam2world(m,ocam_model); % transform from image plane to the camera plane          
-            a1 = M(1)*r(2,1)-M(2)*r(1,1);
-            b1 = M(1)*r(2,2)-M(2)*r(1,2);
-            c1 = M(1)*r(2,3)-M(2)*r(1,3);
-            a2 = M(3)*r(1,1)-M(1)*r(3,1);
-            b2 = M(3)*r(1,2)-M(1)*r(3,2);
-            c2 = M(3)*r(1,3)-M(1)*r(3,3);
+            M = cam2world(m,ocam_model); % transform from image plane to the camera plane  
+            a1 = M(3)*r(1,1)-M(1)*r(3,1);
+            b1 = M(3)*r(1,2)-M(1)*r(3,2);
+            c1 = M(3)*r(1,3)-M(1)*r(3,3);        
+            a2 = M(1)*r(2,1)-M(2)*r(1,1);
+            b2 = M(1)*r(2,2)-M(2)*r(1,2);
+            c2 = M(1)*r(2,3)-M(2)*r(1,3);
 
             Y = (a2*c1-a1*c2)/(a1*b2-a2*b1);
             X = (-c1-b1*Y)/a1;
